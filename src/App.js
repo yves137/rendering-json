70524516826da7ec4e6a8ef03b14aacd1fef1f62
yves137 <oneg2981@gmail.com>
2023-09-26 13:47:42 +0200

    create rendering project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import {data} from './Data'
+import { CardList } from './CardList';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col h-screen bg-[#E1F7DD] justify-center items-center'>
+     <CardList data={data}/>
     </div>
   );
 }
